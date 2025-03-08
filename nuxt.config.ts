6b0957b79d82ea5ba5430188453c00fd4e7e80ae
JongTae <ljt0721@gmail.com>
2025-03-08 10:39:56 +0900

    feat:env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,13 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      weatherApi: process.env.WEATHER_API
+      weatherApi: process.env.WEATHER_API,
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
     }
   },
   plugins: [
