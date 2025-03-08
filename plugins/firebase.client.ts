1aaab78f0fdeb0c3744e1a1f49c840dd794b1cd1
Lim-JongTae <31271171+Lim-JongTae@users.noreply.github.com>
2025-03-08 10:16:07 +0900

    Update firebase.client.ts

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -3,14 +3,7 @@ import { getFirestore } from "firebase/firestore";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const firebaseConfig = {
-    apiKey: "AIzaSyDuFzkASHv71vgeolW4OxKUVPpH7hn7uKQ",
-    authDomain: "foreset-vuetify.firebaseapp.com",
-    databaseURL: "https://foreset-vuetify-default-rtdb.firebaseio.com",
-    projectId: "foreset-vuetify",
-    storageBucket: "foreset-vuetify.appspot.com",
-    messagingSenderId: "442636137598",
-    appId: "1:442636137598:web:21f11c7fcf89d3a47ed42f",
-    measurementId: "G-C46M0LPZ4C"
+  
   };
   // Initialize Firebase
 const app = initializeApp(firebaseConfig);
